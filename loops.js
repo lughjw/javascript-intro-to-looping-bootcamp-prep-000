29fdaf9b0893e28d9a81eaab2788300114855e89
Lugh Wilson <lughjw@gmail.com>
2019-12-09 02:47:04 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array) {
+  var retArr = []
+  for(i=0;i<25;i++) {
+    retArr[i]=`"I am ${i} strange loop${i === 0 ?'':'s'}."`
+  }
+
+  return retArr
+}
+
+function whileLoop(array) {
+  
+}
+
+function doWhileLoop(num) {
+  
+}
