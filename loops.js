f9b045456f84dc0960866e98e17aa4be95e8b0d3
Lugh Wilson <lughjw@gmail.com>
2019-12-09 03:14:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   //var retArr = []
   for(i=0;i<25;i++) {
     //console.log(`"I am ${i} strange loop${i === 1 ?'':'s'}."`)
-    array.push(`"I am ${i} strange loop${i === 1 ?'':'s'}."`)
+    array.push(`I am ${i} strange loop${i === 1 ?'':'s'}.`)
   }
 
   return array
